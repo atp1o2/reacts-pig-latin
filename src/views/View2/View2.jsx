d0f3fc7312fa906482354461700284dc24bb77b0
atp1o2 <andrewpham8@gmail.com>
2017-06-21 22:39:57 -0700

    Add code view and prep for heroku

--- src/views/View2/View2.jsx
+++ src/views/View2/View2.jsx
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-
-const ViewStyle = styled.div`
-    text-align: center;
-`;
 
 export default class View2 extends Component {
   render () {
     return (
-      <ViewStyle>
-        <h1>View 2</h1>
-      </ViewStyle>
+      <div>
+        <h1>Code</h1>
+        <pre >
+         {`
+  const pigLatinChecker = (word, n) => {
+    const library = ['a', 'e', 'i', 'o', 'u', 'bl', 'br', 'ch', 'ck', 'cl', 'cr', 'dr', 'fl', 'fr', 'gh', 'gl', 'gr', 'ng', 'ph', 'pl', 'pr', 'qu', 'sc', 'sh', 'sk', 'sl', 'sm', 'sn', 'sp', 'st', 'sw', 'th', 'tr', 'tw', 'wh', 'wr', 'sch',  'scr',  "shr",  'sph',  'spl',  'spr',  'squ',  'str'];
+    return library.includes(word.slice(0, n));
+  };
+
+  const pigLatinTranslator = (sentence) => {
+    let translated_sentence = [];
+    let regex = /\\b/;
+    let words = sentence.split(regex).join('|').toLowerCase().split('|');
+
+    words.forEach(word => {
+      if (word.match(regex)) {
+        for (let n = 3; n > 0; n--) {
+          if (pigLatinChecker(word, n)) {
+            if (n === 1) { n = 0 }
+              translated_sentence.push(word.substring(n, word.length) + word.toLowerCase().slice(0, n) + "ay");
+              n = 0;
+          } else if (n === 1) {
+            translated_sentence.push(word.substring(n, word.length) + word.toLowerCase().slice(0, n) + "ay");
+            n = 0;
+          }
+        }
+      } else {
+        translated_sentence.push(word);
+      }
+    });
+    return translated_sentence.join("");
+  };
+
+  export { pigLatinTranslator };
+
+        `}
+        </pre>
+
+      </div>
     );
   }
 }
